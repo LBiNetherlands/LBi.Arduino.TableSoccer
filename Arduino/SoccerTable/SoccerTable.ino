f313097c5de641fc10c5c5a66395e1d047456dde
Jeroen Slor <jeroen.slor@gmail.com>
2014-09-14 16:23:01 +0200

    quick hack to turn off display, still needs some refactoring

--- Arduino/SoccerTable/SoccerTable.ino
+++ Arduino/SoccerTable/SoccerTable.ino
@@ -4,8 +4,13 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 int analogButtonPin = 0;
 int scoreTeamBlue = 0;
 int scoreTeamRed = 0;
+unsigned long prevActionMillis = millis();
 
 void setup() {
+  // set output mode to turn on off contrast
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  
   // set pullup mode for multi button
   pinMode(A0, INPUT);
   digitalWrite(A0, HIGH);
@@ -17,8 +22,11 @@ void setup() {
 void loop() {
   int val = analogRead(analogButtonPin);
   
-  if(val >= 500 && val <= 599)
+  if(val >= 400 && val <= 500)
   {
+    lcd.display();
+    digitalWrite(13, HIGH);
+    
     // button 1 (start game)
     scoreTeamBlue = 0;
     scoreTeamRed = 0;
@@ -26,30 +34,39 @@ void loop() {
     lcd.setCursor(0,0);
     lcd.print("BLUE         RED");
     outputScore();
+    
+    prevActionMillis = millis();
   }
-  if(val >= 400 && val <= 499)
+  if(val >= 300 && val <= 400)
   {
-    // button 2 (score blue)
-    scoreTeamBlue = scoreTeamBlue + 1;    
-    outputScore(); 
-  }
-  if(val >= 300 && val <= 399)
-  {
-    // button 3 (score red)
+    lcd.display();
+    digitalWrite(13, HIGH);
+    
+    // button 2 (score red)
     scoreTeamRed = scoreTeamRed + 1;    
-    outputScore();
+    outputScore(); 
+    
+    prevActionMillis = millis();
   }
-  if(val >= 200 && val <= 299)
+  if(val >= 200 && val <= 300)
   {
-    // button 4 (end game)
-    scoreTeamBlue = 0;
-    scoreTeamRed = 0;
-
-    lcd.clear();
-    lcd.print("Press start");
+    lcd.display();
+    digitalWrite(13, HIGH);
+    
+    // button 3 (score blue)
+    scoreTeamBlue = scoreTeamBlue + 1;        
+    outputScore();
+    
+    prevActionMillis = millis();
   }
   
-  delay(300);
+  if(millis() - prevActionMillis > 10000)
+  {
+    lcd.noDisplay();
+    digitalWrite(13, LOW); 
+  }
+  
+  delay(200);    
 
 }
 
